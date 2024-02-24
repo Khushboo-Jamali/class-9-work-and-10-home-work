c081b6e4f9407360553535a8a8a7fa0c9f4d75f3
khushboo-jamali <jamalikhushboo@gmail.com>
2024-02-24 23:17:09 +0100

    js file

---
+++ index.js
@@ -0,0 +1,31 @@
+// function name() {
+//   let img = document.getElementById("img");
+//   img.src = "img/bulboff.png";
+// }
+
+// function name2() {
+//   img2.src = "img/bulbon.png";
+// }
+// function one() {
+//   let img = document.getElementById("img");
+//   img.src = "img/bulboff.png";
+// }
+// function two() {
+//   // let img = document.getElementById("img");
+//   img.src = "img/bulbon.png";
+// }
+function one() {
+  document.write(
+    "<img src='./img/Picture2.jpg' style='height: 200px; width: 200px' alt='' />"
+  );
+}
+function two() {
+  document.write(
+    "<img src='./img/Picture4.jpg'  style='height: 200px; width: 200px' alt='' />"
+  );
+}
+function three() {
+  document.write(
+    "<img src='./img/Picture6.jpg'  style='height: 200px; width: 200px' alt='' />"
+  );
+}
