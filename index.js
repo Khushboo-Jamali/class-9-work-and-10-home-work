3f3a072f03abd23e160b7f126b32d6669bfc4e58
khushboo-jamali <jamalikhushboo@gmail.com>
2024-02-26 19:11:59 +0100

    starting

--- index.js
+++ index.js
@@ -15,27 +15,37 @@
 //   img.src = "img/bulbon.png";
 // }
 function one() {
-  document.write(
-    "<img src='./img/Picture2.jpg' style='height: 200px; width: 200px' alt='' />"
-  );
+  // document.write(
+  //   "<img src='./img/Picture2.jpg' style='height: 200px; width: 200px' alt='' />"
+  // );
+  let img = document.getElementById("img");
+  img.src = "img/Picture2.jpg";
 }
 function two() {
-  document.write(
-    "<img src='./img/Picture4.jpg'  style='height: 200px; width: 200px' alt='' />"
-  );
+  // document.write(
+  //   "<img src='./img/Picture4.jpg'  style='height: 200px; width: 200px' alt='' />"
+  // );
+  let img = document.getElementById("img");
+  img.src = "img/Picture4.jpg";
 }
 function three() {
-  document.write(
-    "<img src='./img/Picture6.jpg'  style='height: 200px; width: 200px' alt='' />"
-  );
+  // document.write(
+  //   "<img src='./img/Picture6.jpg'  style='height: 200px; width: 200px' alt='' />"
+  // );
+  let img = document.getElementById("img");
+  img.src = "img/Picture6.jpg";
 }
 function four() {
-  document.write(
-    "<img src='./img/bike.png'  style='height: 200px; width: 200px' alt='' />"
-  );
+  // document.write(
+  //   "<img src='./img/bike.png'  style='height: 200px; width: 200px' alt='' />"
+  // );
+  let img = document.getElementById("img");
+  img.src = "img/bike.png";
 }
 function five() {
-  document.write(
-    "<img src='./img/car.jpg'  style='height: 200px; width: 200px' alt='' />"
-  );
+  // document.write(
+  //   "<img src='./img/car.jpg'  style='height: 200px; width: 200px' alt='' />"
+  // );
+  let img = document.getElementById("img");
+  img.src = "img/car.jpg";
 }
